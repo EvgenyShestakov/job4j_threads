160e5df514e410e126c687cee08e304f0cfb59cd
EvgenyShestakov <lester_g@inbox.ru>
2021-08-17 10:01:22 +0300

    3. Прерывание нити.

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,32 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        String[] strings = new String[3];
+        strings[0] = "\\";
+        strings[1] = "|";
+        strings[2] = "/";
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + strings[index]);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            index++;
+            if (index == 3) {
+                index = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1000);
+        progress.interrupt();
+    }
+}
+
