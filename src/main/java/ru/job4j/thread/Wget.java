39ee0a0655136d85bcc279114037b5e5fe4e671e
EvgenyShestakov <lester_g@inbox.ru>
2021-08-20 09:44:26 +0300

    4. Скачивание файла с ограничением.

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -6,32 +6,33 @@ import java.io.IOException;
 import java.net.URL;
 
 public class Wget implements Runnable {
-    private final String url;
+    private final String urlIn;
+    private final String urlOut;
     private final int speed;
 
-    public Wget(String url, int speed) {
-        this.url = url;
+    public Wget(String urlIn, String urlOut, int speed) {
+        this.urlIn = urlIn;
+        this.urlOut = urlOut;
         this.speed = speed;
     }
 
     @Override
     public void run() {
-        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(urlIn).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(urlOut)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             long finish;
-            long result = 0;
+            long result;
             long start = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                if (result == 0) {
-                    finish = System.currentTimeMillis();
-                    result = finish - start;
-                }
+                finish = System.currentTimeMillis();
+                result = finish - start;
                 if (result < speed) {
                     Thread.sleep(speed - result);
                 }
+                start = System.currentTimeMillis();
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -39,9 +40,13 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Arguments set incorrectly");
+        }
+        String urlIn = args[0];
+        String urlOut = args[1];
+        int speed = Integer.parseInt(args[2]);
+        Thread wget = new Thread(new Wget(urlIn, urlOut, speed));
         wget.start();
         wget.join();
     }
