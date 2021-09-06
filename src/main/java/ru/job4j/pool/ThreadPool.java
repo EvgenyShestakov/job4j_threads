e1c19f9ff688c989a8093a0ad419d60913786abc
EvgenyShestakov <lester_g@inbox.ru>
2021-09-06 14:18:31 +0300

    1. Реализовать ThreadPool.

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,66 @@
+package ru.job4j.pool;
+
+import ru.job4j.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
+
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
+    }
+
+    public void shutdown() {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+
+    public List<Thread> getThreads() {
+        return threads;
+    }
+
+    public SimpleBlockingQueue<Runnable> getTasks() {
+        return tasks;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool();
+        List<Thread> threads = threadPool.getThreads();
+        SimpleBlockingQueue<Runnable> tasks = threadPool.getTasks();
+        int size = Runtime.getRuntime().availableProcessors();
+        for (int index = 0; index < size; index++) {
+            threads.add(new Thread(() -> {
+                try {
+                    while (!tasks.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        Runnable job = tasks.poll();
+                        job.run();
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (int index = 0; index < 20; index++) {
+            threadPool.work(() -> {
+                System.out.println("Beginning of work " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("End of work " + Thread.currentThread().getName());
+            });
+        }
+        Thread.sleep(2000);
+        threadPool.shutdown();
+    }
+}
