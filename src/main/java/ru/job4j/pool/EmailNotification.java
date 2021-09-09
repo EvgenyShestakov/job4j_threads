473dadb93a17ed6daeb4291e92e0d30d7d57e6ee
EvgenyShestakov <lester_g@inbox.ru>
2021-09-09 11:26:43 +0300

    2. ExecutorService рассылка почты.

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-    ExecutorService poll = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+   private final ExecutorService poll = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
         poll.submit(() -> {
@@ -18,6 +18,13 @@ public class EmailNotification {
 
     public void close() {
         poll.shutdown();
+        while (!poll.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) {
